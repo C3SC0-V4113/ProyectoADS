e567d79198af9e6f8ef1a073fac2eb783cc943dc
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-03-25 20:40:09 -0600

    Formulario RegistroAlumnos
    Se agrego nuevo Formulario para registrar alumnos a la base de datos

---
+++ Proyec-ADS/Proyec-ADS/frm_RegistroAlumnos.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Proyec_ADS
+{
+    partial class frm_registroAlumnos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_Carnet = new System.Windows.Forms.Label();
+            this.lbl_nombre = new System.Windows.Forms.Label();
+            this.lbl_apellido = new System.Windows.Forms.Label();
+            this.lbl_correo = new System.Windows.Forms.Label();
+            this.lbl_carrera = new System.Windows.Forms.Label();
+            this.txt_carnet = new System.Windows.Forms.TextBox();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
+            this.txt_apellido = new System.Windows.Forms.TextBox();
+            this.txt_correo = new System.Windows.Forms.TextBox();
+            this.cmb_carrera = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lbl_Carnet
+            // 
+            this.lbl_Carnet.AutoSize = true;
+            this.lbl_Carnet.Location = new System.Drawing.Point(57, 54);
+            this.lbl_Carnet.Name = "lbl_Carnet";
+            this.lbl_Carnet.Size = new System.Drawing.Size(38, 13);
+            this.lbl_Carnet.TabIndex = 0;
+            this.lbl_Carnet.Text = "Carnet";
+            // 
+            // lbl_nombre
+            // 
+            this.lbl_nombre.AutoSize = true;
+            this.lbl_nombre.Location = new System.Drawing.Point(296, 50);
+            this.lbl_nombre.Name = "lbl_nombre";
+            this.lbl_nombre.Size = new System.Drawing.Size(44, 13);
+            this.lbl_nombre.TabIndex = 1;
+            this.lbl_nombre.Text = "Nombre";
+            // 
+            // lbl_apellido
+            // 
+            this.lbl_apellido.AutoSize = true;
+            this.lbl_apellido.Location = new System.Drawing.Point(592, 51);
+            this.lbl_apellido.Name = "lbl_apellido";
+            this.lbl_apellido.Size = new System.Drawing.Size(44, 13);
+            this.lbl_apellido.TabIndex = 2;
+            this.lbl_apellido.Text = "Apellido";
+            // 
+            // lbl_correo
+            // 
+            this.lbl_correo.AutoSize = true;
+            this.lbl_correo.Location = new System.Drawing.Point(57, 121);
+            this.lbl_correo.Name = "lbl_correo";
+            this.lbl_correo.Size = new System.Drawing.Size(38, 13);
+            this.lbl_correo.TabIndex = 3;
+            this.lbl_correo.Text = "Correo";
+            // 
+            // lbl_carrera
+            // 
+            this.lbl_carrera.AutoSize = true;
+            this.lbl_carrera.Location = new System.Drawing.Point(296, 118);
+            this.lbl_carrera.Name = "lbl_carrera";
+            this.lbl_carrera.Size = new System.Drawing.Size(41, 13);
+            this.lbl_carrera.TabIndex = 4;
+            this.lbl_carrera.Text = "Carrera";
+            // 
+            // txt_carnet
+            // 
+            this.txt_carnet.Location = new System.Drawing.Point(142, 51);
+            this.txt_carnet.Name = "txt_carnet";
+            this.txt_carnet.Size = new System.Drawing.Size(100, 20);
+            this.txt_carnet.TabIndex = 5;
+            // 
+            // txt_nombre
+            // 
+            this.txt_nombre.Location = new System.Drawing.Point(353, 50);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(121, 20);
+            this.txt_nombre.TabIndex = 6;
+            // 
+            // txt_apellido
+            // 
+            this.txt_apellido.Location = new System.Drawing.Point(661, 50);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(100, 20);
+            this.txt_apellido.TabIndex = 7;
+            // 
+            // txt_correo
+            // 
+            this.txt_correo.Location = new System.Drawing.Point(142, 118);
+            this.txt_correo.Name = "txt_correo";
+            this.txt_correo.Size = new System.Drawing.Size(100, 20);
+            this.txt_correo.TabIndex = 8;
+            // 
+            // cmb_carrera
+            // 
+            this.cmb_carrera.FormattingEnabled = true;
+            this.cmb_carrera.Location = new System.Drawing.Point(353, 115);
+            this.cmb_carrera.Name = "cmb_carrera";
+            this.cmb_carrera.Size = new System.Drawing.Size(121, 21);
+            this.cmb_carrera.TabIndex = 9;
+            // 
+            // frm_registroAlumnos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(950, 551);
+            this.Controls.Add(this.cmb_carrera);
+            this.Controls.Add(this.txt_correo);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.txt_carnet);
+            this.Controls.Add(this.lbl_carrera);
+            this.Controls.Add(this.lbl_correo);
+            this.Controls.Add(this.lbl_apellido);
+            this.Controls.Add(this.lbl_nombre);
+            this.Controls.Add(this.lbl_Carnet);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frm_registroAlumnos";
+            this.Text = "frm_RegistroAlumnos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_Carnet;
+        private System.Windows.Forms.Label lbl_nombre;
+        private System.Windows.Forms.Label lbl_apellido;
+        private System.Windows.Forms.Label lbl_correo;
+        private System.Windows.Forms.Label lbl_carrera;
+        private System.Windows.Forms.TextBox txt_carnet;
+        private System.Windows.Forms.TextBox txt_nombre;
+        private System.Windows.Forms.TextBox txt_apellido;
+        private System.Windows.Forms.TextBox txt_correo;
+        private System.Windows.Forms.ComboBox cmb_carrera;
+    }
+}
