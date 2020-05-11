a5277e3f90bb653f0e9f8be9c05a91d9101fbdf3
ElinGeniero02 <pinochofino@gmail.com>
2020-05-11 11:36:54 -0600

    Alumno -Agregar

---
+++ Proyec-ADS/Proyec-ADS/Alumno.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+namespace Proyec_ADS
+{
+    class Alumno
+    {
+        private string CodCarnet;
+        private string NombreAlumno;
+        private string ApellidoAlumno;
+        private string CorreoAlumno;
+        public string codCarnet
+        {
+            get { return CodCarnet; }
+            set { CodCarnet = value; }
+        }
+        public string nombreAlumno
+        {
+            get { return NombreAlumno; }
+            set { NombreAlumno = value; }
+        }
+
+        public string apellidoAlumno
+        {
+            get { return ApellidoAlumno; }
+            set { ApellidoAlumno = value; }
+        }
+        public string correoAlumno
+        {
+            get { return CorreoAlumno; }
+            set { CorreoAlumno = value; }
+        }
+        private Conexion conex = new Conexion();
+        SqlCommand comando = new SqlCommand();
+        SqlDataReader lector;
+
+        public void AgregarAlmno(string codCarnet, string nombreAlumno, string apellidoAlumno, string correoAlumno)
+        {
+            comando.Connection = conex.AbrirConexion();
+            comando.CommandText = "CrearEstudiantes";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.Clear();
+            comando.Parameters.AddWithValue("@CodCarnet", codCarnet);
+            comando.Parameters.AddWithValue("@NombreAlumno", nombreAlumno);
+            comando.Parameters.AddWithValue("@ApellidoAlumno", apellidoAlumno);
+            comando.Parameters.AddWithValue("@CorreoAlumno",correoAlumno);
+            
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conex.CerrarConexion();
+        }
+
+    }
+}
--- Proyec-ADS/Proyec-ADS/frm_registroAlumnos.cs
+++ Proyec-ADS/Proyec-ADS/frm_registroAlumnos.cs
@@ -16,15 +16,33 @@ namespace Proyec_ADS
         {
             InitializeComponent();
         }
-
+        Alumno al = new Alumno();
         private void lbl_Carnet_Click(object sender, EventArgs e)
         {
 
         }
 
+        public void limp() {
+            txt_carnet.Clear();
+            txt_nombre.Clear();
+            txt_correo.Clear();
+            txt_apellido.Clear();
+        }
+
         private void btn_registraralumno_Click(object sender, EventArgs e)
         {
+            try
+            {
+                al.AgregarAlmno(txt_carnet.Text, txt_nombre.Text, txt_apellido.Text, txt_correo.Text);
+                MessageBox.Show("Alumno Guardado con Exito, se requiere inicio de sesión");
+                limp();
 
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error :", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void frm_registroAlumnos_Load(object sender, EventArgs e)
